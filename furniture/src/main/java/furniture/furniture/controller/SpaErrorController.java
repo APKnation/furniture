5b83fbd2678d826c4e5e6e271a957e5f0557c931
apknation <atanasikafuka@gmail.com>
2026-06-15 10:29:04 +0300

    docfile

--- furniture/src/main/java/furniture/furniture/controller/SpaErrorController.java
+++ furniture/src/main/java/furniture/furniture/controller/SpaErrorController.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class SpaErrorController {
+import org.springframework.boot.web.servlet.error.ErrorController;
 
-    @RequestMapping("/error")
+public class SpaErrorController implements ErrorController {
+
+    @RequestMapping("/error/spa")
     public String handleError() {
         // Forward to the index.html for client-side routing to take over
         return "forward:/index.html";
